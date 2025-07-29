6b1a6e519c1be0863e0e24c6da062c7397ce035c
USUARIO <antholozadarequelme@gmail.com>
2025-07-29 02:25:07 -0500

    Creacion de Main para la clase Operacion, junto al objeto instanciado de la Clase Operacion, donde enviaremos los datos de num1 y num2 para su posterior calculo y visualizacion con el metodo de mostrarResultados

---
+++ src/ClassAndObjects/MainOp.java
@@ -0,0 +1,18 @@
+﻿package ClassAndObjects;
+
+import javax.swing.*;
+
+public class MainOp {
+    public static void main(String[] args) {
+        int num1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer numero: "));
+        int num2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo numero: "));
+
+        Operacion op = new Operacion(); // Crear una instancia de la clase Operacion
+        op.sumar(num1, num2); // Llamar al método sumar
+        op.restar(num1, num2); // Llamar al método restar
+        op.multiplicar(num1, num2); // Llamar al método multiplicar
+        op.dividir(num1, num2); // Llamar al método dividir
+
+        op.mostrarResultados(); // Mostrar los resultados de las operaciones
+    }
+}
