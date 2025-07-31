e589a370c9390d843354b1404b54435e186fd1ec
USUARIO <antholozadarequelme@gmail.com>
2025-07-31 16:32:04 -0500

    Creacion del atributo String dni y constructor Persona dni

--- src/metodosPersona/Persona.java
+++ src/metodosPersona/Persona.java
@@ -4,10 +4,17 @@ public class Persona {
     // Atributos
     String nombre;
     int edad;
+    String dni;
 
     // Métodos
     public Persona(int edad, String nombre) {
         this.edad = edad;
         this.nombre = nombre;
     }
+
+    public Persona(String dni) {
+        this.dni = dni;
+    }
+
+
 }
