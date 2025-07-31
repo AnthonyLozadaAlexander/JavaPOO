50ac7996358aae040fa1744a89bfc3ecfecfa71b
USUARIO <antholozadarequelme@gmail.com>
2025-07-31 16:23:05 -0500

    Creacion de atributos y metodo constructor

--- src/metodosPersona/Persona.java
+++ src/metodosPersona/Persona.java
@@ -1,6 +1,13 @@
 package metodosPersona;
 
 public class Persona {
+    // Atributos
+    String nombre;
+    int edad;
 
-
+    // Métodos
+    public Persona(int edad, String nombre) {
+        this.edad = edad;
+        this.nombre = nombre;
+    }
 }
