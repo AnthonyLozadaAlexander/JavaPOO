399532d4ea3d542f453bcba4ff2e9e2ff41d7f0c
USUARIO <antholozadarequelme@gmail.com>
2025-08-06 04:16:09 -0500

    Añadir método main a la clase Main para crear y utilizar instancias de Persona

--- src/metodosPersona/Main.java
+++ src/metodosPersona/Main.java
@@ -1,4 +1,12 @@
 package metodosPersona;
 
 public class Main {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Alejandro", 21);
+        persona1.correr();
+
+        Persona persona2 = new Persona("095485853");
+        persona2.correr(100);
+
+    }
 }
