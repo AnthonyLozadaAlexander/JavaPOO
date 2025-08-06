841dbc848dc60e908250a3f0a93fdd4ee79b00fd
USUARIO <antholozadarequelme@gmail.com>
2025-08-06 04:15:32 -0500

    Añadir constructor a la clase vehiculo para inicializar atributos

--- src/ejercicioVehiculos/vehiculo.java
+++ src/ejercicioVehiculos/vehiculo.java
@@ -5,5 +5,11 @@ public class vehiculo {
     private String modelo;
     private double precio;
 
+    public vehiculo(double precio, String modelo, String marca) {
+        this.precio = precio;
+        this.modelo = modelo;
+        this.marca = marca;
+    }
+
 
 }
