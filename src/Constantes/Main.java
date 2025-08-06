13dbc1bef4f30f0450e963765a941be2e988ecb3
USUARIO <antholozadarequelme@gmail.com>
2025-08-06 04:16:20 -0500

    Añadir clase Main con método main para crear y modificar instancias de Persona

---
+++ src/Constantes/Main.java
@@ -0,0 +1,9 @@
+package Constantes;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Juan", 25);
+        persona1.setEdad(22); // Modificar la edad
+        persona1.mostrarDatos();
+    }
+}
