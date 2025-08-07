13c2778051f6b146977b6b14debf0a39050df402
USUARIO <antholozadarequelme@gmail.com>
2025-08-07 00:20:08 -0500

    Añadir importación de Scanner y declarar variables para marca, modelo y precio en el método main

--- src/ejercicioVehiculos/Main.java
+++ src/ejercicioVehiculos/Main.java
@@ -1,7 +1,13 @@
 package ejercicioVehiculos;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String marca, modelo;
+        double precio;
+
 
     }
 }
