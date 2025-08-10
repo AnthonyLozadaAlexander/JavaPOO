a83aee7b3b929043643c440aeef49f4ceb10f9c7
USUARIO <antholozadarequelme@gmail.com>
2025-08-10 01:43:48 -0500

    Añadir clase Atletas para gestionar información de atletas y sus tiempos de carrera

---
+++ src/ejercicioAtletismo/atletas.java
@@ -0,0 +1,21 @@
+package ejercicioAtletismo;
+
+public class atletas {
+    private String codigoAtleta;
+    private String nombreAtleta;
+    private double tiempoCarrera;
+
+    public atletas(String _codigoAtleta, String _nombreAtleta, double _tiempoCarrera){
+        this.codigoAtleta = _codigoAtleta;
+        this.nombreAtleta = _nombreAtleta;
+        this.tiempoCarrera = _tiempoCarrera;
+    }
+
+    public double getTiempoCarrera() {
+        return tiempoCarrera;
+    }
+
+    public String mostrarDatos(){
+        return "Codigo Atleta: " + codigoAtleta + " // Nombre Atleta: " + nombreAtleta + " // Tiempo Carrera: " + String.format("%.2f", tiempoCarrera) + " segundos.";
+    }
+}
