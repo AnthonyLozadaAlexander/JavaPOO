93c9303b14f3b020e2fbeb6c861e2415080e0190
USUARIO <antholozadarequelme@gmail.com>
2025-08-10 01:53:16 -0500

    Se creo las variables para el codigo del atleta, nombre del atleta, tiempo de carrera, tamaño del arreglo de la cantidad de atletas, index del atleta de menor tiempo y index del atleta de mayor tiempo

--- src/ejercicioAtletismo/Main.java
+++ src/ejercicioAtletismo/Main.java
@@ -1,7 +1,14 @@
 package ejercicioAtletismo;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    String codigoAtleta, nombreAtleta;
+    double tiempoCarrera;
+    int numsAtletas, indexMenorTiempo, indexMayorTiempo;
+
 
     }
 }
