16657a8277f048dfbcd53e0cad28e0f1aa4799f3
USUARIO <antholozadarequelme@gmail.com>
2025-08-10 02:14:10 -0500

    Añadir entrada de datos para cada atleta en el arreglo durante la inicialización

--- src/ejercicioAtletismo/Main.java
+++ src/ejercicioAtletismo/Main.java
@@ -14,7 +14,15 @@ public class Main {
         atletas atleta[] = new atletas[numsAtletas];
 
         for (int i = 0; i < atleta.length ; i++) {
-            
+            input.nextLine(); // Limpiar el buffer
+            System.out.println("Ingrese el codigo del atleta["+i+"]: ");
+            codigoAtleta = input.nextLine();
+            System.out.println("Ingrese el nombre del atleta["+i+"]: ");
+            nombreAtleta = input.nextLine();
+            System.out.println("Ingrese el tiempo de carrera del atleta["+i+"]: ");
+            tiempoCarrera = input.nextDouble();
+
+            atleta[i] = new atletas(codigoAtleta, nombreAtleta, tiempoCarrera);
         }
 
 
