42a4cc14a804b4ac7942e59f7f0ee94d8ab37357
USUARIO <antholozadarequelme@gmail.com>
2025-08-10 02:11:35 -0500

    Añadir bucle para inicializar el arreglo de atletas en el método main

--- src/ejercicioAtletismo/Main.java
+++ src/ejercicioAtletismo/Main.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-    String codigoAtleta, nombreAtleta;
-    double tiempoCarrera;
-    int numsAtletas, indexMenorTiempo, indexMayorTiempo;
+        String codigoAtleta, nombreAtleta;
+        double tiempoCarrera;
+        int numsAtletas, indexMenorTiempo, indexMayorTiempo;
 
         System.out.println("¿Cuantos atletas participarán en la carrera?");
         numsAtletas = input.nextInt();
         atletas atleta[] = new atletas[numsAtletas];
+
+        for (int i = 0; i < atleta.length ; i++) {
+            
+        }
 
 
     }
