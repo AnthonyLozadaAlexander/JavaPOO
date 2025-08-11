889eecd5a35e73fb2d52890d18f8e6e3b472076f
USUARIO <antholozadarequelme@gmail.com>
2025-08-10 23:12:55 -0500

    Agregar instancia de Estudiante y mostrar datos en el método main

--- src/ejercicioHerencia/Main.java
+++ src/ejercicioHerencia/Main.java
@@ -2,6 +2,8 @@ package ejercicioHerencia;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Estudiante estudiante = new Estudiante("Juan", "Perez", 21, 123, 8.5);
+        estudiante.mostrarDatos();
+
     }
 }
