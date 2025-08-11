ea5019c56cd7c2e840e1b8082fe1da6c23225825
USUARIO <antholozadarequelme@gmail.com>
2025-08-10 22:55:53 -0500

    Agregar clase Estudiante que extiende a Persona con atributos y constructor

--- src/ejercicioHerencia/Estudiante.java
+++ src/ejercicioHerencia/Estudiante.java
@@ -1,4 +1,19 @@
 package ejercicioHerencia;
 
-public class Estudiante {
+public class Estudiante extends Persona {
+    //Atributos extras
+    private int codigoEstudiante;
+    private double notaFinal;
+
+    //Constructor
+    public Estudiante(String _nombre, String _apellido, int _edad, int _codigoEstudiante, double _notaFinal){
+        super(_nombre, _apellido, _edad); // atributos de la clase padre
+        this.codigoEstudiante = _codigoEstudiante;
+        this.notaFinal = _notaFinal;
+    }
+
+    public void mostrarDatos(){
+
+    }
+
 }
