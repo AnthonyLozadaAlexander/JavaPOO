1f8f2cb66e5326353cdb2fc8b0c4799080be5345
USUARIO <antholozadarequelme@gmail.com>
2025-08-14 21:24:31 -0500

    Agregar instancia de Vaca y llamada al método comer en el método main

--- src/SobreEscrituraDeMetodos/Main.java
+++ src/SobreEscrituraDeMetodos/Main.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Persona persona = new Persona();
         Perro perro = new Perro();
+        Vaca vaca = new Vaca();
 
         persona.comer();
         perro.comer();
+        vaca.comer();
 
     }
 }
