a51c97462c0d1a4c121017a4d63bf38c3b9047a3
USUARIO <antholozadarequelme@gmail.com>
2025-08-14 21:21:14 -0500

    Modificar el método comer en la clase Persona para mejorar la descripción de la acción

--- src/SobreEscrituraDeMetodos/Persona.java
+++ src/SobreEscrituraDeMetodos/Persona.java
@@ -4,6 +4,6 @@ public class Persona extends Animal{
 
     @Override
     public void comer(){
-        System.out.println("Estoy comiendo como una persona");
+        System.out.println("Estoy comiendo sentado y cubiertos");
     }
 }
