4af81f9ef4991922f18d0e04ee373a65e976eed3
USUARIO <antholozadarequelme@gmail.com>
2025-08-14 21:16:01 -0500

    Agregar clase Perro que extiende Animal y sobreescribe el método comer

---
+++ src/SobreEscrituraDeMetodos/Perro.java
@@ -0,0 +1,9 @@
+package SobreEscrituraDeMetodos;
+
+public class Perro extends Animal{
+
+    @Override
+    public void comer(){
+        System.out.println("Estoy comiendo en mi plato con croquetas");
+    }
+}
