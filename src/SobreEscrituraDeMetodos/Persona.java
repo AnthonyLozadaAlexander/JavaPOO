1fc534c7b9451c5a0f3359ef9773a0660d84a0e5
USUARIO <antholozadarequelme@gmail.com>
2025-08-14 21:14:03 -0500

    Agregar clase Persona que extiende Animal y sobreescribe el método comer

---
+++ src/SobreEscrituraDeMetodos/Persona.java
@@ -0,0 +1,9 @@
+package SobreEscrituraDeMetodos;
+
+public class Persona extends Animal{
+
+    @Override
+    public void comer(){
+        System.out.println("Estoy comiendo como una persona");
+    }
+}
