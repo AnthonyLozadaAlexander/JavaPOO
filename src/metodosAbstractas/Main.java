b0ea477208ed05c8546dbc01884b76b5aed10477
zDefcon <antholozadarequelme@gmail.com>
2025-08-17 00:56:10 -0500

    Instantiate AnimalHerviboro in Main and call alimentarse

--- src/metodosAbstractas/Main.java
+++ src/metodosAbstractas/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Planta planta = new Planta();
         AnimalCarnivoro animalCarnivoro = new AnimalCarnivoro();
+        AnimalHerviboro animalHerviboro = new AnimalHerviboro();
 
         animalCarnivoro.alimentarse();
         planta.alimentarse();
+        animalHerviboro.alimentarse();
     }
 }
