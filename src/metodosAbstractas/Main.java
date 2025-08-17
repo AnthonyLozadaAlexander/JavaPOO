97ac7c283c531efd987c1028bb12c917a37e89ad
zDefcon <antholozadarequelme@gmail.com>
2025-08-17 00:20:55 -0500

    Instanciar AnimalCarnivoro en Main y llamar a alimentarse

--- src/metodosAbstractas/Main.java
+++ src/metodosAbstractas/Main.java
@@ -3,6 +3,9 @@ package metodosAbstractas;
 public class Main {
     public static void main(String[] args) {
         Planta planta = new Planta();
+        AnimalCarnivoro animalCarnivoro = new AnimalCarnivoro();
+
+        animalCarnivoro.alimentarse();
         planta.alimentarse();
     }
 }
