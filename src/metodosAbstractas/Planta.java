d97af2395fff3817633d7c547dd85749345324a8
zDefcon <antholozadarequelme@gmail.com>
2025-08-17 00:01:03 -0500

    Agregar clase Planta que extiende serVivo y sobrescribe el método alimentarse

---
+++ src/metodosAbstractas/Planta.java
@@ -0,0 +1,9 @@
+package metodosAbstractas;
+
+public class Planta extends serVivo {
+
+    @Override
+    public void alimentarse(){
+        System.out.println("La planta se alimenta atraves de la fotosintesis");
+    }
+}
