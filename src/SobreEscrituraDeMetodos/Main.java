dc8c929a7255d04393864720370b2f1238c7fc4d
zDefcon <antholozadarequelme@gmail.com>
2025-08-22 01:40:53 -0500

    Instantiate Tiburon and Gato in Main and call comer method

--- src/SobreEscrituraDeMetodos/Main.java
+++ src/SobreEscrituraDeMetodos/Main.java
@@ -5,10 +5,14 @@ public class Main {
         Persona persona = new Persona();
         Perro perro = new Perro();
         Vaca vaca = new Vaca();
+        Tiburon tiburon = new Tiburon();
+        Gato gato = new Gato();
 
         persona.comer();
         perro.comer();
         vaca.comer();
+        tiburon.comer();
+        gato.comer();
 
     }
 }
