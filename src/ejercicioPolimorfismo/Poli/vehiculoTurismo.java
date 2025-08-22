ce27d464bc7eff25740bd5ae1e4607f991615063
zDefcon <antholozadarequelme@gmail.com>
2025-08-22 01:54:36 -0500

    feat: Añadir clase vehiculoTurismo con constructor y atributo nPuertas

---
+++ src/ejercicioPolimorfismo/Poli/vehiculoTurismo.java
@@ -0,0 +1,10 @@
+package ejercicioPolimorfismo.Poli;
+
+public class vehiculoTurismo extends Vehiculo {
+    private int nPuertas;
+
+    public vehiculoTurismo(String matricula, String marca, String modelo, int nPuertas) {
+        super(matricula, marca, modelo);
+        this.nPuertas = nPuertas;
+    }
+}
