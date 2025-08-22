03b4c20f9c01083ad226ece25a5e98dee223646a
zDefcon <antholozadarequelme@gmail.com>
2025-08-22 01:40:58 -0500

    Instantiate Tiburon and Gato in Main and call comer method

--- src/persona/Persona.java
+++ src/persona/Persona.java
@@ -2,16 +2,20 @@ package persona;
 
 public class Persona {
 
+    // Atributos
     String nombre;
+    String apellido;
     int edad;
 
-    public Persona(String _nombre, int _edad){
+    public Persona(String _nombre, String _apellido, int _edad) {
         nombre = _nombre;
         edad = _edad;
+        apellido = _apellido;
     }
 
     public void mostrarDatos() {
         System.out.println("\nNombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
         System.out.println("Edad: " + edad);
     }
 
