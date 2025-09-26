02fdff3fd107e2f996729d09469bf40a931aa944
zDefcon <antholozadarequelme@gmail.com>
2025-09-25 19:11:45 -0500

    feat: Añadir clase Estudiante con atributos y métodos correspondientes

---
+++ src/EstudianteIGU/Estudiante.java
@@ -0,0 +1,57 @@
+package EstudianteIGU;
+
+public class Estudiante {
+    private String nombre;
+    private int edad;
+    private String carrera;
+    private double nota1;
+    private double nota2;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public String mostrarDatos() {
+        return "Nombre: " + nombre + "\n" +
+                "Edad: " + edad + "\n" +
+                "Carrera: " + carrera + "\n" +
+                "Nota 1: " + nota1 + "\n" +
+                "Nota 2: " + nota2 + "\n";
+    }
+}
