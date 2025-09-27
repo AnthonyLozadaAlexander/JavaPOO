b285fe7b47f4fd01f023c50d98821dd772c47e84
zDefcon <antholozadarequelme@gmail.com>
2025-09-26 23:58:38 -0500

    feat: Añadir clase ProductoClass con métodos para calcular subtotal y descuento

---
+++ src/Factura/ProductoClass.java
@@ -0,0 +1,39 @@
+package Factura;
+
+public class ProductoClass {
+    private String producto;
+    private double precioUnitario;
+    private int cantidad;
+    private double precioTotal;
+    private double descuento;
+    private final double IVA = 0.15;
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setPrecioTotal(double precioTotal) {
+        this.precioTotal = precioTotal;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+
+    public double calcularSubtotal() {
+        return precioUnitario * cantidad;
+    }
+
+    public double calcularDescuento() {
+        double descuento = calcularSubtotal() * this.descuento;
+        return descuento;
+    }
+}
